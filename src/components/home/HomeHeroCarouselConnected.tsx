e1865e3519864bc8f0ec9607dc1c0e6048c8f61c
Boatsy007 <rr.hinchcliffe@gmail.com>
2026-07-25 18:43:05 +1000

    Apply mobile fix to number one hero card

--- src/components/home/HomeHeroCarouselConnected.tsx
+++ src/components/home/HomeHeroCarouselConnected.tsx
@@ -1,10 +1,14 @@
 import { useLocation } from 'react-router-dom'
 import { useHomeData } from './useHomeData'
 import HomeHeroCarousel from './HomeHeroCarousel'
+import HomeHeroNumberOneMobileFix from './HomeHeroNumberOneMobileFix'
 
 export default function HomeHeroCarouselConnected() {
   const { pathname } = useLocation()
   const home = useHomeData()
   if (pathname !== '/') return null
-  return <HomeHeroCarousel top={home.entries[0]} updatedAt={home.generatedAt} />
+  return <>
+    <HomeHeroCarousel top={home.entries[0]} updatedAt={home.generatedAt} />
+    <HomeHeroNumberOneMobileFix />
+  </>
 }
