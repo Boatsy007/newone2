0717bdd44b7e61f94b16665ea07dbfd971913c2d
Boatsy007 <rr.hinchcliffe@gmail.com>
2026-07-25 17:53:25 +1000

    Remove leftover original hero from carousel

--- src/components/home/HomeHeroCarouselConnected.tsx
+++ src/components/home/HomeHeroCarouselConnected.tsx
@@ -1,14 +1,10 @@
 import { useLocation } from 'react-router-dom'
 import { useHomeData } from './useHomeData'
 import HomeHeroCarousel from './HomeHeroCarousel'
-import HomeHeroOriginalRanking from './HomeHeroOriginalRanking'
 
 export default function HomeHeroCarouselConnected() {
   const { pathname } = useLocation()
   const home = useHomeData()
   if (pathname !== '/') return null
-  return <>
-    <HomeHeroCarousel top={home.entries[0]} updatedAt={home.generatedAt} />
-    <HomeHeroOriginalRanking />
-  </>
+  return <HomeHeroCarousel top={home.entries[0]} updatedAt={home.generatedAt} />
 }
