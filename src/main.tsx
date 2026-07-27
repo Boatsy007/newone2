2fd4fa6f69d0d9545442cf6893c32769cf72cfa5
Boatsy007 <rr.hinchcliffe@gmail.com>
2026-07-27 14:35:01 +1000

    mount league portal users route

--- src/main.tsx
+++ src/main.tsx
@@ -51,6 +51,7 @@ import LeaguePortalDashboard from './pages/LeaguePortalDashboard.tsx'
 import LeaguePortalProfile from './pages/LeaguePortalProfile.tsx'
 import LeaguePortalNews from './pages/LeaguePortalNews.tsx'
 import LeaguePortalSponsors from './pages/LeaguePortalSponsors.tsx'
+import LeaguePortalUsers from './pages/LeaguePortalUsers.tsx'
 import ClubPortalDashboard from './pages/ClubPortalDashboard.tsx'
 import ClubPortalTeamSelection from './pages/ClubPortalTeamSelection.tsx'
 import ClubPortalNews from './pages/ClubPortalNews.tsx'
@@ -130,6 +131,7 @@ createRoot(document.getElementById('root')!).render(
     <Route path="/league-portal/:leagueId/profile" element={<LeaguePortalProfile/>}/>
     <Route path="/league-portal/:leagueId/news" element={<LeaguePortalNews/>}/>
     <Route path="/league-portal/:leagueId/sponsors" element={<LeaguePortalSponsors/>}/>
+    <Route path="/league-portal/:leagueId/users" element={<LeaguePortalUsers/>}/>
     <Route path="/league-portal/:leagueId" element={<LeaguePortalDashboard/>}/>
     <Route path="/club-portal/:clubId/team-selection" element={<ClubPortalTeamSelection/>}/>
     <Route path="/club-portal/:clubId/news" element={<ClubPortalNews/>}/>
